f00dd0375cd156bc822e7e1fbda2eb93463db601
vollov.ca <dike.zhang@gmail.com>
2018-05-30 13:56:40 -0400

    add i18n

--- src/main/java/ca/eve/app/demo/controller/HelloController.java
+++ src/main/java/ca/eve/app/demo/controller/HelloController.java
@@ -1,5 +1,8 @@
 package ca.eve.app.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +11,10 @@ import ca.eve.app.demo.domain.Hello;
 
 @RestController
 public class HelloController {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping(path = "/hello")
 	public Hello Hello() {
 		return new Hello("Hello World");
@@ -16,5 +23,11 @@ public class HelloController {
 	@GetMapping(path = "/hello/{name}")
 	public Hello helloWorldPathVariable(@PathVariable String name) {
 		return new Hello(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path = "/hello-i18n")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, 
+									LocaleContextHolder.getLocale());
 	}
 }
---
+++ src/main/java/ca/eve/app/demo/controller/UserController.java
@@ -0,0 +1,83 @@
+package ca.eve.app.demo.controller;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import ca.eve.app.demo.dao.UserRepository;
+import ca.eve.app.demo.domain.User;
+import ca.eve.app.demo.exceptions.UserNotFoundException;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@GetMapping("/jpa/users")
+	public List<User> retrieveAllUsers() {
+		return userRepository.findAll();
+	}
+	
+	@DeleteMapping("/jpa/users/{id}")
+	public void deleteUser(@PathVariable int id) {
+		userRepository.deleteById(id);
+	}
+
+	@GetMapping("/jpa/users/{id}")
+	public Resource<User> retrieveUser(@PathVariable int id) {
+		Optional<User> user = userRepository.findById(id);
+
+		if (!user.isPresent())
+			throw new UserNotFoundException("id-" + id);
+
+		// "all-users", SERVER_PATH + "/users"
+		// retrieveAllUsers
+		Resource<User> resource = new Resource<User>(user.get());
+
+		ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+		resource.add(linkTo.withRel("all-users"));
+
+		// HATEOAS
+
+		return resource;
+	}
+
+
+	//
+	// input - details of user
+	// output - CREATED & Return the created URI
+
+	// HATEOAS
+
+	@PostMapping("/jpa/users")
+	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+		
+		System.out.println("add user" + user.toString());
+		User savedUser = userRepository.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
+
+	}
+}
